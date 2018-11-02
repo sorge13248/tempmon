a7817bb85c1f7c71c9f2cfc35fbc41471bf1a37d
Francesco Sorge <4579797+sorge13248@users.noreply.github.com>
2018-11-02 17:15:20 +0100

    Added support for custom server URL and custom token. Configured two artifacts.

--- src/com/francescosorge/java/JsonFromInternet.java
+++ src/com/francescosorge/java/JsonFromInternet.java
@@ -6,6 +6,7 @@ import com.google.gson.JsonParser;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -13,7 +14,10 @@ class JsonFromInternet {
     private String url;
     private JsonObject json;
 
-    JsonFromInternet(String pUrl) {
+    public JsonFromInternet() {
+    }
+
+    public JsonFromInternet(String pUrl) throws MalformedURLException {
         this.url = pUrl;
 
         // Connect to the URL using java's native library
@@ -26,13 +30,11 @@ class JsonFromInternet {
             JsonParser jp = new JsonParser(); //from gson
             JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); // Convert the input stream to a json element
             this.json = root.getAsJsonObject(); //May be an array, may be an object.
+        } catch (MalformedURLException e) {
+            throw new MalformedURLException();
         } catch (Exception e) {
             System.out.println("An unknown error has just occurred. Error: " + e.toString());
         }
-    }
-
-    public String getUrl() {
-        return url;
     }
 
     @Override
@@ -40,7 +42,15 @@ class JsonFromInternet {
         return this.json.toString();
     }
 
-    JsonObject getJson() {
-        return json;
+    public String getValue(String field) {
+        try {
+            return this.json.get(field).getAsString();
+        }catch(Exception e) {
+            return "An unknown error has just occurred. Error: " + e.toString();
+        }
+    }
+
+    public boolean hasKey(String field) {
+        return this.json.has(field);
     }
 }
--- src/com/francescosorge/java/Main.java
+++ src/com/francescosorge/java/Main.java
@@ -8,13 +8,64 @@ import com.profesorfalken.jsensors.model.sensors.Fan;
 import com.profesorfalken.jsensors.model.sensors.Temperature;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final double VERSION = 0.1;
+    private static final String defaultURL = "http://localhost/temp-mon";
 
     public static void main(String[] args) {
-        JsonFromInternet tempMonSettings = new JsonFromInternet("http://localhost/temp-mon/retrieve-data?token=fgj80tyw45hcgf83w4hfw34asds");
+        // Header
+        System.out.println("TempMob - A simple temperature monitor for your device");
+        System.out.println("Developed by Francesco Sorge (www.francescosorge.com) - Version " + VERSION);
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+        // Provide URL
+        boolean validURL = false;
+        String URL;
+
+        do { // Ask for an URL until a valid one is provided
+            System.out.print("TempMon server URL (default = " + defaultURL + "): ");
+            URL = scanner.nextLine();
+            if (URL.equals("")) URL = defaultURL; // If no URL is typed, it assumes to use the default one
+            System.out.println("Trying to establish connection with TempMon server at " + URL);
+
+            try { // Tries to connect with server
+                JsonFromInternet tempMonServer = new JsonFromInternet(URL + "/sw-info");
+                validURL = true;
+                System.out.println("Success! Connection with TempMon server established correctly.");
+                System.out.println("Server is running version " + tempMonServer.getValue("version")); // Everything went OK and server version is printed on screen
+            } catch (java.net.MalformedURLException e) {
+                System.out.println("Error! An invalid URL has been provided."); // Invalid URL provided
+            }
+        }while(!validURL);
+
+        System.out.println(); // Adds a blank row
+
+        // Provide token
+        boolean validToken = false;
+        JsonFromInternet tempMonSettings = new JsonFromInternet();
+        do {
+            System.out.print("Type your access token: "); // Asks for access token
+            String token = scanner.nextLine();
+
+            try { // Tries to use access token to get user settings
+                tempMonSettings = new JsonFromInternet(URL + "/retrieve-data?token=" + token);
+            } catch (Exception e) {
+                System.out.println("Error " + e);
+                System.exit(-1);
+            }
+
+            if (tempMonSettings.hasKey("user_id")) { // If 'user_id' key is present, token was valid so it moves to next steps
+                validToken = true;
+            } else {
+                System.out.println("Error! Invalid token provided."); // Token not valid
+            }
+        }while(!validToken);
+
         System.out.println(tempMonSettings.toString());
-        System.out.println(tempMonSettings.getJson());
+        System.out.println(tempMonSettings.getValue("cpu-max-temperature"));
         System.exit(0);
 
 
